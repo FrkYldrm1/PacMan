7ceeb145f1839ecb4a07b806f2772be92820eabc
Faruk yildirim <fy222av@student.lnu.se>
2025-01-13 22:05:10 +0100

    lOAD MAP FIX

--- src/PacMan.java
+++ src/PacMan.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Random;
 import javax.swing.*;
 
-public class PacMan extends JPanel{
+public class PacMan extends JPanel {
     class Block {
         int x;
         int y;
@@ -26,17 +26,16 @@ public class PacMan extends JPanel{
 
         }
 
-
     }
+
     private int rowCount = 21;
     private int columnCount = 19;
     private int tileSize = 32;
     private int boardWidht = columnCount * tileSize;
     private int boardHeight = rowCount * tileSize;
 
-
     private Image wallImage;
-    private Image blueGhowsImage;
+    private Image blueGhostImage;
     private Image orangeGhostImage;
     private Image pinkGhostImage;
     private Image redGhostImage;
@@ -75,7 +74,7 @@ public class PacMan extends JPanel{
     HashSet<Block> foods;
     HashSet<Block> ghosts;
     Block pacman;
-    
+
     Timer gameLoop;
     char[] directions = { 'U', 'D', 'L', 'R' }; // up down left right
     Random random = new Random();
@@ -83,15 +82,13 @@ public class PacMan extends JPanel{
     int lives = 3;
     boolean gameOver = false;
 
-
-
     PacMan() {
         setPreferredSize(new Dimension(boardWidht, boardHeight));
         setBackground(Color.BLACK);
 
         // Load images
         wallImage = new ImageIcon(getClass().getResource("./wall.png")).getImage();
-        blueGhowsImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
+        blueGhostImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
         orangeGhostImage = new ImageIcon(getClass().getResource("./orangeGhost.png")).getImage();
         pinkGhostImage = new ImageIcon(getClass().getResource("./pinkGhost.png")).getImage();
         redGhostImage = new ImageIcon(getClass().getResource("./redGhost.png")).getImage();
@@ -101,22 +98,49 @@ public class PacMan extends JPanel{
         pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
         pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
 
+        loadMap();
+        System.out.println(walls.size());
+        System.out.println(foods.size());
+        System.out.println(ghosts.size());
 
     }
 
-    public void loadMap(){
+    public void loadMap() {
         walls = new HashSet<Block>();
         foods = new HashSet<Block>();
         ghosts = new HashSet<Block>();
 
-        for (int r =0; r < rowCount; r++){
-            for (int c = 0; c < columnCount; c++){
+        for (int r = 0; r < rowCount; r++) {
+            for (int c = 0; c < columnCount; c++) {
+                String row = tileMap[r];
+                char tileMapChar = row.charAt(c);
 
+                int x = c * tileSize;
+                int y = r * tileSize;
+
+                if (tileMapChar == 'X') { // block wall
+                    Block wall = new Block(wallImage, x, y, tileSize, tileSize);
+                    walls.add(wall);
+                } else if (tileMapChar == 'b') { // blue ghost
+                    Block ghost = new Block(blueGhostImage, x, y, tileSize, tileSize);
+                    ghosts.add(ghost);
+                } else if (tileMapChar == 'o') { // orange ghost
+                    Block ghost = new Block(orangeGhostImage, x, y, tileSize, tileSize);
+                    ghosts.add(ghost);
+                } else if (tileMapChar == 'p') { // pink ghost
+                    Block ghost = new Block(pinkGhostImage, x, y, tileSize, tileSize);
+                    ghosts.add(ghost);
+                } else if (tileMapChar == 'r') { // red ghost
+                    Block ghost = new Block(redGhostImage, x, y, tileSize, tileSize);
+                    ghosts.add(ghost);
+                } else if (tileMapChar == 'P') { // pacman
+                    pacman = new Block(pacmanRightImage, x, y, tileSize, tileSize);
+                } else if (tileMapChar == ' ') { // food
+                    Block food = new Block(null, x + 14, y + 14, 4, 4);
+                    foods.add(food);
+                }
+            }
         }
-
-
-
     }
 
-}
 }
